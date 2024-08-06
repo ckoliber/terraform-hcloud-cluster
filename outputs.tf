fb5c8cfcc8c214327bc9dc6a61ba18b05c383b41
KoLiBer <koliberr136a1@gmail.com>
2024-08-06 13:13:58 +0000

    feat: add load_balancers output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,17 @@
-output "connections" {
+output "servers" {
   value       = { for key, val in terraform_data.this : key => val.output }
   sensitive   = false
-  description = "Cluster Connections"
+  description = "Cluster Servers"
+}
+
+output "load_balancers" {
+  value = {
+    for key, val in hcloud_load_balancer.this : key => {
+      public_ipv4 = val.ipv4
+      public_ipv6 = val.ipv6
+      private_ip  = try(hcloud_load_balancer_network.this[key].ip, null)
+    }
+  }
+  sensitive   = false
+  description = "Cluster Load Balancers"
 }
